8d810022c136b517c3f4e4e1e81ba6b2e894eab6
Noah Davey <noah.davey@gmail.com>
2021-03-06 23:50:43 +1000

    start of pythagorean-triplet

---
+++ javascript/pythagorean-triplet/pythagorean-triplet.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Pythagorean Triplet' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export function triplets({ minFactor, maxFactor, sum }) {
+  console.log(minFactor);
+  console.log(maxFactor);
+  console.log(sum);
+  throw new Error('Remove this statement and implement this function');
+}
+
+class Triplet {
+  constructor(a, b, c) {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  get toArray() {
+    throw new Error('Remove this statement and implement this function');
+  }
+}
